5506c13fbce82bb2ad435957812b4c5929663837
gavrielsatrio <gavrielsatrio@gmail.com>
2024-10-26 19:33:30 +0700

    feat: add core container component

---
+++ src/components/core/Container.tsx
@@ -0,0 +1,9 @@
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement>{}
+
+export function Container({ className, children }: ContainerProps) {
+    return (
+        <div className={`container mx-auto ${className}`}>
+            {children}
+        </div>
+    )
+}
