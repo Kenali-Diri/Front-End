fab0f7007e7a981f7981f0a22f3638ac17f3a651
gavrielsatrio <gavrielsatrio@gmail.com>
2024-10-26 19:32:38 +0700

    feat: add tailwind color config and server config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,20 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: '1.5rem'
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'blue': '#435BC0',
+        'pink': '#F6666F',
+        'barbie-pink': '#FF9FCD',
+        'soft-cream': '#FFF1E3',
+        'dark-slate': '#464555',
+        'medium-slate': '#AAA9BC',
+        'light-slate': '#CECDE1'
       },
     },
   },
