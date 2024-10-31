4665758a006ade36bef09c1d08fac8def6d98bf7
KevinChiputra <kevin.chiputra@binus.ac.id>
2024-10-31 18:21:42 +0700

    backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,29 @@
+'use client'
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5206/WeatherForecast'); // Example public API
+        const result = await response.json();
+        setData(result); // Store the result in state
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log('data', data)
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
