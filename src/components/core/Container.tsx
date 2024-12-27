d4b709b82875a9db3b38ecc3138d75dc166cbf46
KevinChiputra <kevin.chiputra@binus.ac.id>
2024-12-27 13:19:56 +0700

    create dialogGagal for boss level

--- src/components/core/Container.tsx
+++ src/components/core/Container.tsx
@@ -1,9 +1,5 @@
-interface ContainerProps extends React.HTMLAttributes<HTMLDivElement>{}
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function Container({ className = '', children }: ContainerProps) {
-    return (
-        <div className={`container mx-auto ${className}`}>
-            {children}
-        </div>
-    )
+    return <div className={`container mx-auto ${className} `}>{children}</div>;
 }
